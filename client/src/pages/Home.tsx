a31e52b233a3097bfbe8b487259f1d28748bed51
peace-ishimwe <peaceishimwem@gmail.com>
2023-09-10 18:20:14 +0200

    portfolio

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/header/Header"
+import Main from "../components/home/Main"
 
 const Home = () => {
   return (
     <div className="main-home-container">
       <Header />
+      <Main />
     </div>
   )
 }
