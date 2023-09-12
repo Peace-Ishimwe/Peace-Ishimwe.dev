0c8ce83a5ae121e87dbfe224efa2b6f881616139
peace-ishimwe <peaceishimwem@gmail.com>
2023-09-12 12:38:41 +0200

    pt

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,32 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        'main-dark': '#121120'
-      }
-    },
-  },
-  plugins: [],
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
+
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
+			},
+			container: {
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '5rem',
+					xl: '6rem',
+					'2xl': '8rem',
+				},
+			},
+		},
+	},
+	variants: {
+		extend: { opacity: ['disabled'] },
+	},
+	plugins: ['@tailwindcss/forms'],
 };
