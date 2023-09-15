0c7c9e14d85309e5ebcf366da2bc7a2b5b2d959e
peace-ishimwe <peaceishimwem@gmail.com>
2023-09-15 21:15:42 +0200

    �  modified the manifest.json

--- client/src/components/contact/ContactDetails.jsx
+++ client/src/components/contact/ContactDetails.jsx
@@ -3,17 +3,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Gikondo, Kicukiro, Rwanda',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'peaceishimwem@gmail.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '+250 793 092 863',
 		icon: <FiPhone />,
 	},
 ];
