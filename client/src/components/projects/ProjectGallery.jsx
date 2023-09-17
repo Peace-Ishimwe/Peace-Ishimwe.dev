daec4d7cb5664b884a3a211fc9c1ea23ec78d778
peace-ishimwe <peaceishimwem@gmail.com>
2023-09-17 23:36:56 +0200

    single projects share

--- client/src/components/projects/ProjectGallery.jsx
+++ client/src/components/projects/ProjectGallery.jsx
@@ -22,7 +22,13 @@ const ProjectGallery = () => {
 					</div>
 				);
 			})}
-
+			<FacebookShareButton
+				url={'https://peace-ishimwe.vercel.app/projects/single-project/0'}
+				quote={'Title or jo bhi aapko likhna ho'}
+				hashtag={'#portfolio...'}
+			>
+				<FacebookIcon size={40} round={true} />
+			</FacebookShareButton>
 		</div>
 	);
 };
