5a64f01e953bf24b677f5914509f0acf0e2eef68
peace-ishimwe <peaceishimwem@gmail.com>
2023-09-26 14:16:41 +0200

    added three js

--- client/src/components/Doings/Doings.jsx
+++ client/src/components/Doings/Doings.jsx
@@ -1,85 +1,66 @@
-import React from 'react'
-import IconWeb from '../../images/Doings/icon-dev.svg'
-import IconApp from '../../images/Doings/icon-app.svg'
-import IconDesign from '../../images/Doings/icon-design.svg'
-import IconEmbeded from '../../images/Doings/icon-photo.svg'
+import React from 'react';
+
+// Import your icons
+import IconWeb from '../../images/Doings/icon-dev.svg';
+import IconApp from '../../images/Doings/icon-app.svg';
+import IconDesign from '../../images/Doings/icon-design.svg';
+import IconEmbeded from '../../images/Doings/icon-photo.svg';
+
+// Define an array of services
+const services = [
+  {
+    icon: IconWeb,
+    title: 'Web development',
+    description: 'High-quality development of sites at the professional level.',
+  },
+  {
+    icon: IconApp,
+    title: 'Mobile apps',
+    description: 'Professional development of applications for iOS and Android.',
+  },
+  {
+    icon: IconDesign,
+    title: 'Web design',
+    description: 'The most modern and high-quality design made at a professional level.',
+  },
+  {
+    icon: IconEmbeded,
+    title: 'Embeded systems',
+    description: 'Unlock endless tech opportunities by learning embedded systems design and development.',
+  },
+];
 
 const Doings = () => {
-    return (
-        <section className="service text-primary-dark dark:text-ternary-light">
+  return (
+    <section className="service text-primary-dark dark:text-ternary-light">
+      <h3 className="h3 service-title font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light text-center">
+        What I'm Doing
+      </h3>
+      <ul className="service-list mt-[3rem]">
+        {services.map((service, index) => (
+          <li
+            key={index}
+            className="service-item bg-secondary-light dark:bg-ternary-dark !shadow-lg hover:!shadow-xl flex gap-[2rem]"
+          >
+            <div className="service-icon-box">
+              <img
+                src={service.icon}
+                alt={`${service.title} icon`}
+                width="40"
+                className="min-w-[40px]"
+              />
+            </div>
+            <div className="service-content-box !text-start">
+              <h4 className="h4 service-item-title font-semibold text-xl text-ternary-dark dark:text-ternary-light">
+                {service.title}
+              </h4>
+              <p className="service-item-text text-lg font-medium">{service.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
-            <h3 className="h3 service-title font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light text-center">What i'm doing</h3>
-
-            <ul className="service-list mt-[3rem]">
-
-                <li className="service-item bg-secondary-light dark:bg-ternary-dark !shadow-lg hover:!shadow-xl flex gap-[2rem]">
-
-                    <div className="service-icon-box">
-                        <img src={IconWeb} alt="Web development icon" width="40" className='min-w-[40px]' />
-                    </div>
-
-                    <div className="service-content-box !text-start">
-                        <h4 className="h4 service-item-title font-semibold text-xl text-ternary-dark dark:text-ternary-light">Web development</h4>
-
-                        <p className="service-item-text text-lg font-medium">
-                            High-quality development of sites at the professional level.
-                        </p>
-                    </div>
-
-                </li>
-
-                <li className="service-item bg-secondary-light dark:bg-ternary-dark !shadow-lg hover:!shadow-xl flex gap-[2rem]">
-
-                    <div className="service-icon-box">
-                        <img src={IconApp} alt="mobile app icon" width="40" className='min-w-[40px]' />
-                    </div>
-
-                    <div className="service-content-box !text-start">
-                        <h4 className="h4 service-item-title font-semibold text-xl text-ternary-dark dark:text-ternary-light">Mobile apps</h4>
-
-                        <p className="service-item-text text-lg font-medium">
-                            Professional development of applications for iOS and Android.
-                        </p>
-                    </div>
-
-                </li>
-
-                <li className="service-item bg-secondary-light dark:bg-ternary-dark !shadow-lg hover:!shadow-xl flex gap-[2rem]">
-
-                    <div className="service-icon-box">
-                        <img src={IconDesign} alt="design icon" width="40" className='min-w-[40px]' />
-                    </div>
-
-                    <div className="service-content-box !text-start">
-                        <h4 className="h4 service-item-title font-semibold text-xl text-ternary-dark dark:text-ternary-light">Web design</h4>
-
-                        <p className="service-item-text text-lg font-medium">
-                            The most modern and high-quality design made at a professional level.
-                        </p>
-                    </div>
-
-                </li>
-
-                <li className="service-item bg-secondary-light dark:bg-ternary-dark !shadow-lg hover:!shadow-xl flex gap-[2rem]">
-
-                    <div className="service-icon-box">
-                        <img src={IconEmbeded} alt="camera icon" width="40" className='min-w-[40px]' />
-                    </div>
-
-                    <div className="service-content-box !text-start">
-                        <h4 className="h4 service-item-title font-semibold text-xl text-ternary-dark dark:text-ternary-light">Embeded systems</h4>
-
-                        <p className="service-item-text text-lg font-medium">
-                            Unlock endless tech opportunities by learning embedded systems design and development.
-                        </p>
-                    </div>
-
-                </li>
-
-            </ul>
-
-        </section>
-    )
-}
-
-export default Doings
+export default Doings;
--- client/src/components/shared/AppBanner.jsx
+++ client/src/components/shared/AppBanner.jsx
@@ -1,6 +1,7 @@
 import { FiArrowDownCircle } from 'react-icons/fi';
-import DeveloperHome from '../../images/developer-home.svg';
+// import DeveloperHome from '../../images/developer-home.svg';
 import { motion } from 'framer-motion';
+import { ComputersCanvas } from '../canvas';
 
 const AppBanner = () => {
 
@@ -9,9 +10,9 @@ const AppBanner = () => {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-			className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
+			className="flex flex-col items-center lg:flex-row mt-12 md:mt-2"
 		>
-			<div className="w-full md:w-1/3 text-left">
+			<div className="lg:w-[28%] lg:absolute z-50 text-left">
 				<motion.h1
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
@@ -20,7 +21,7 @@ const AppBanner = () => {
 						duration: 0.9,
 						delay: 0.1,
 					}}
-					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light "
+					className="font-general-semibold text-2xl lg:text-3xl xl:text-4xl text-center lg:text-left text-ternary-dark dark:text-primary-light whitespace-nowrap"
 				>
 					Hi, I am Peace Ishimwe
 				</motion.h1>
@@ -32,7 +33,7 @@ const AppBanner = () => {
 						duration: 0.9,
 						delay: 0.2,
 					}}
-					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-500 dark:text-gray-200"
+					className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center lg:text-left leading-normal text-gray-500 dark:text-gray-200"
 				>
 					 A Highly Skilled And Motivated Fullstack Developer
 				</motion.p>
@@ -44,12 +45,12 @@ const AppBanner = () => {
 						duration: 0.9,
 						delay: 0.3,
 					}}
-					className="flex justify-center sm:block"
+					className="flex justify-center lg:block"
 				>
 					<a
 						download="Peace-Ishimwe-Resume.pdf"
 						href="/files/Peace-Ishimwe-Resume.pdf"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-500 focus:ring-1 hover:bg-indigo-600 text-gray-100 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
 						<FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
@@ -62,14 +63,10 @@ const AppBanner = () => {
 			<motion.div
 				initial={{ opacity: 0, y: -180 }}
 				animate={{ opacity: 1, y: 0 }}
-				transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-				className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
+				transition={{ ease: 'easeInOut', duration: 1.5, delay: .5 }}
+				className="w-full h-[15rem] md:h-[30rem] float-right mt-8 sm:mt-0"
 			>
-				<img
-					src={DeveloperHome}
-					alt="Developer"
-					className='lg:float-right'
-				/>
+				<ComputersCanvas />
 			</motion.div>
 		</motion.section>
 	);
