1e5d0fdf6b43044ca7bbce833b0274d2b4af55fa
peace-ishimwe <peaceishimwem@gmail.com>
2023-10-13 11:59:55 +0200

    added the skeleton loading

--- client/src/components/about/AboutMeBio.jsx
+++ client/src/components/about/AboutMeBio.jsx
@@ -1,35 +1,39 @@
-import profileImage from '../../images/ProfileMe/Profile7.jpg';
-import { useContext } from 'react';
+import React, { useState, useEffect , useContext } from 'react';
+import Skeleton from '@mui/material/Skeleton';
 import AboutMeContext from '../../context/AboutMeContext';
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-
+import profileImage from '../../images/ProfileMe/Profile7.jpg';
 
 const AboutMeBio = () => {
-	const { aboutMe } = useContext(AboutMeContext);
+    const { aboutMe } = useContext(AboutMeContext);
+    const [imageLoaded, setImageLoaded] = useState(false);
 
-	return (
-		<div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
-			<div className="w-full sm:w-1/4 mb-7 sm:mb-0">
-				{/* <img src={profileImage} className="rounded-lg w-96" alt="" /> */}
-				<LazyLoadImage
-					alt={"profile"}
-					effect="blur"
-					src={profileImage} 
-				/>
-			</div>
+    useEffect(() => {
+        const image = new Image();
+        image.src = profileImage;
+        image.onload = () => {
+            setImageLoaded(true);
+        };
+    }, []);
 
-			<div className="font-general-regular w-full sm:w-3/4 text-left">
-				{aboutMe.map((bio) => (
-					<p
-						className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
-						key={bio.id}
-					>
-						{bio.bio}
-					</p>
-				))}
-			</div>
-		</div>
-	);
+    return (
+        <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+            <div className="w-full sm:w-1/4 mb-7 sm:mb-0">
+                {imageLoaded ? (
+                    <img src={profileImage} className="rounded-lg w-96" alt="Profile" />
+                ) : (
+                    <Skeleton variant="rect" width={384} height={512} animation="wave" />
+                )}
+            </div>
+
+            <div className="font-general-regular w-full sm:w-3/4 text-left">
+                {aboutMe.map((bio) => (
+                    <p className="mb-4 text-ternary-dark dark:text-ternary-light text-lg" key={bio.id}>
+                        {bio.bio}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default AboutMeBio;
